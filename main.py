c324de88f33669f67b779f3b9bb760ef2aacf34c
AtthaphanPak <129380493+AtthaphanPak@users.noreply.github.com>
2024-02-19 16:55:21 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import pprint
 
+url = "https://docs.google.com/spreadsheets/d/1WJEOfHH5TXH1-EuWHqPptbt4tVG43VXqwBnzkqbujbQ/edit#gid=0"
 scope = ['https://www.googleapis.com/auth/spreadsheets']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('keys.json', scope) #ชื่อไฟล์ต้องเขียนให้ตรงกับที่โหลดมา
 client = gspread.authorize(credentials)
-sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1WJEOfHH5TXH1-EuWHqPptbt4tVG43VXqwBnzkqbujbQ/edit#gid=0") #ใส่ url ของหน้า Google Sheets
+sheet = client.open_by_url(url) #ใส่ url ของหน้า Google Sheets
 worksheet = sheet.get_worksheet(0)
 results = worksheet.get_all_records()
-pp = pprint.PrettyPrinter()
-pp.pprint(results)
+print(results)
+#print(results[-1]["UV"])
+#pp = pprint.PrettyPrinter()
+#pp.pprint(results)
